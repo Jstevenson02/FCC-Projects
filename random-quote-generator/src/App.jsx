11f74b61d4003018058409c9452751ae8d8dc4d5
jstevenson02 <jacobryanstevenson@gmail.com>
2023-05-17 17:41:18 -0500

    first commit

---
+++ random-quote-generator/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="quote-box">
+          <div className="quote">Quote Here</div>
+          <button className="quote-btn">New Quote</button>
+        </div>
+      </div>
+      <div className="author">by jstevenson02</div>
+    </>
+  );
+}
+
+export default App;
