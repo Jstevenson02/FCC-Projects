6038e179abc1f9c80ff7fdb92ab1f8014a46b6cb
jstevenson02 <jacobryanstevenson@gmail.com>
2023-05-24 13:19:50 -0500

    styling it up!

--- markdown-previewer/src/App.jsx
+++ markdown-previewer/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { marked } from "marked";
+import "./App.css";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      markdown: "",
+      markdown: placeholder,
     };
   }
 
@@ -15,22 +16,69 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <>
-        <div className="App">
-          <div className="container">
-            <textarea
-              className="input"
-              value={this.state.markdown}
-              onChange={(e) => {
-                this.updateMarkdown(e.target.value);
-              }}
-            ></textarea>
-            <div
-              dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
-            ></div>
-          </div>
+      <div className="App">
+        <div className="container">
+          <div className="header">Editor</div>
+          <textarea
+            spellCheck="false"
+            className="input"
+            value={this.state.markdown}
+            onChange={(e) => {
+              this.updateMarkdown(e.target.value);
+            }}
+          ></textarea>
+          <div className="header">Previewer</div>
+          <div
+            className="output"
+            dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
+          ></div>
         </div>
-      </>
+      </div>
     );
   }
 }
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
