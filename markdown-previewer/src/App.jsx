40abe4c22290906cfb70e926ad5336a1026b9dd1
jstevenson02 <jacobryanstevenson@gmail.com>
2023-05-24 12:00:09 -0500

    functionallity added

--- markdown-previewer/src/App.jsx
+++ markdown-previewer/src/App.jsx
@@ -1,7 +1,36 @@
-import "./App.css";
+import React from "react";
+import { marked } from "marked";
 
-function App() {
-  return <></>;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: "",
+    };
+  }
+
+  updateMarkdown(markdown) {
+    this.setState({ markdown });
+  }
+
+  render() {
+    return (
+      <>
+        <div className="App">
+          <div className="container">
+            <textarea
+              className="input"
+              value={this.state.markdown}
+              onChange={(e) => {
+                this.updateMarkdown(e.target.value);
+              }}
+            ></textarea>
+            <div
+              dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
+            ></div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
